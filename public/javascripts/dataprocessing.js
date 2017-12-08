fe7c7f9cfe3f07649f74ca87a1f3d1af78078378
bingbinghu <1157962465@qq.com>
2017-12-08 16:33:40 +0800

    '3.5'

--- public/javascripts/dataprocessing.js
+++ public/javascripts/dataprocessing.js
@@ -1,27 +1,28 @@
-let idquery=function(){
-        let idqueryvalue=document.getElementById('idqueryvalue').value;
-        if(isNaN(parseInt(idqueryvalue))){
-            document.getElementById('idqueryresult').innerHTML='格式错误！';
-            return 0;
-        }
-        let idqueryresult=document.getElementById('idqueryresult');
-        let xmlhttp=new XMLHttpRequest();
-        xmlhttp.open('GET','idquery?id='+idqueryvalue,true);
-        xmlhttp.send();
-        xmlhttp.onreadystatechange=function(){
-            if(xmlhttp.readyState===4){
-                if(xmlhttp.status===200){
-                    idqueryresult.innerHTML=xmlhttp.responseText;
-                }else{
-                    console.log('ERROR:'+'status:'+xmlhttp.status+','+'state:'+xmlhttp.readyState);
-                }
+let idquery;
+idquery = function () {
+    let idqueryvalue = document.getElementById('idqueryvalue').value;
+    if (isNaN(parseInt(idqueryvalue))) {
+        document.getElementById('idqueryresult').innerHTML = '格式错误！';
+        return 0;
+    }
+    let idqueryresult = document.getElementById('idqueryresult');
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', 'idquery?id=' + idqueryvalue, true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4) {
+            if (xmlhttp.status === 200) {
+                idqueryresult.innerHTML = xmlhttp.responseText;
+            } else {
+                console.log('ERROR:' + 'status:' + xmlhttp.status + ',' + 'state:' + xmlhttp.readyState);
             }
         }
     }
+};
 
 let entersubmit=function(){
     event.keyCode===13?idquery():false;
-}
+};
 
 let addsubmit=function(){
     let namesubmitvalue=document.getElementById('namesubmitvalue').value;
@@ -47,7 +48,7 @@ let addsubmit=function(){
         }
     }
 
-}
+};
 
 let update=function(){
     let idupdatevalue=document.getElementById('idupdatevalue').value;
@@ -79,4 +80,4 @@ let update=function(){
             }
         }
     }
-}
+};
