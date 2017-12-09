5dfa88a2261e9417e5620e366e9758184b0dc074
bingbinghu <1157962465@qq.com>
2017-12-09 15:25:26 +0800

    '4'

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.get('/idquery', function(req, res) {
     let idquerysqlway='SELECT * FROM user WHERE ID='+idqueryvalue;
     connection.query(idquerysqlway,function(err,result){
         if(result.length===1){
-            let birthdayformat=new Date(result[0].birthday).getFullYear()+'-'+new Date(result[0].birthday).getMonth()+'-'+new Date(result[0].birthday).getDay();
+            let birthdayformat=new Date(result[0].birthday).getFullYear()+'-'+(new Date(result[0].birthday).getMonth()+1)+'-'+new Date(result[0].birthday).getDate();
             idqueryresult='ID:'+result[0].ID+'   姓名:'+result[0].name+'   密码:'+result[0].password+'   生日:'+birthdayformat;
             return res.send(idqueryresult);
         }else{
