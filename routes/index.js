7f00ae8303d1ce3d5b1162bea0d257b853691ae6
intbingbing <1157962465@qq.com>
2018-01-07 17:43:14 +0800

    修复

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ let mysql=require('mysql');
 let connection=mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'123456',
+    password:'',
     database:'test'
 });
 connection.connect();
@@ -58,7 +58,7 @@ router.post('/addsubmit',function(req,res){
             res.send('服务器内部错误！');
             return;
         }
-        let addsubmitresult='ID:'+result.insertId+' Name:'+req.body.name;
+        let addsubmitresult=[{id:result.insertId,name:req.body.name}];
         res.send(addsubmitresult);
     });
 });
