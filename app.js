45263da3fc3039945d9a987e62ffac897ca92eed
bingbinghu <1157962465@qq.com>
2018-02-21 21:02:24 +0800

    。。。

--- app.js
+++ app.js
@@ -22,31 +22,38 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials","true");  //服务端允许携带cookie
+    res.header("Access-Control-Allow-Origin", req.headers.origin);  //允许的访问域
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");  //访问头
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  //访问方法
     res.header("X-Powered-By",' 3.2.1');
-
-    next();
+    res.header("Content-Type", "application/json;charset=utf-8");
+    if (req.method == 'OPTIONS') {
+        res.header("Access-Control-Max-Age", 86400);
+        res.sendStatus(204); //让options请求快速返回
+    }
+    else {
+        next();
+    }
 });
 app.use('/', index);
 app.use('/users', users);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,144 +2,124 @@ let express = require('express');
 let path=require('path');
 let router = express.Router();
 let mysql=require('mysql');
-var util = require('util');
-/* GET home page. */
+let md5 = require('crypto-js/md5');
+let hex = require('crypto-js/enc-hex');
+//兼容前期connection.query()代码
+let { auth } = require('../src/authCustom.js');
+let fs=require('fs');
+let sqlDefine = require('../database/sqlDefine.js')
 
-let connection=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'test'
+router.get('/test',function (req,res) {
+    res.set('content-type','image/jpeg')
+    return res.sendFile('/usr/local/nginx/html/ftp/root.jpg');
+})
+
+router.get('/checkCookie', function(req,res,next) {
+    if(Object.keys(req.cookies).length===0){
+        return res.send({statusCode:'400114',tag:0});
+    }else{
+        req.body=req.cookies;
+        sqlDefine.crudAuth(req,res,next);
+    }
 });
-connection.connect();
-setInterval(function(){
-    connection.query('SELECT 1');
-},5000);
-setInterval(function(){
-    console.log(new Date().toISOString());
-},1800000);
+
+router.get('/clear_cookie', async function(req, res) {
+    console.log('clear_cookie');
+    res.clearCookie('username',{path:'/'});
+    res.clearCookie('secret',{path:'/'});
+    return res.send({statusCode:'200131',tag:1});
+});
 
 router.get('/', function(req, res) {
     let indexpath=path.resolve(__dirname, '../public/page');
     res.set('Content-Type', 'text/html');
     res.sendFile(indexpath+'/index.html');
 });
-// get查询
-router.get('/idquery', function(req, res) {
-    //for(let key in req.cookies){
-    //    console.log('Cookies:  '+key+':'+req.cookies[key])
-    //}
-    //console.log('Cookie:'+req.cookies)
-    let idqueryresult='未查询到数据！';
-    let idqueryvalue=parseInt(req.query.id);
-    let idquerysqlway='SELECT * FROM user WHERE ID='+idqueryvalue;
-    connection.query(idquerysqlway,function(err,result){
-        if(result.length===1){
-            let birthdayformat=new Date(result[0].birthday).getFullYear()+'-'+(new Date(result[0].birthday).getMonth()+1)+'-'+new Date(result[0].birthday).getDate();
-            idqueryresult=[{id:result[0].ID,name:result[0].name,password:result[0].password,birthday:birthdayformat}];
-            return res.send(idqueryresult);
-        }else{
-            return res.send(idqueryresult);
-        }
-    });
-});
-// post增加
-router.post('/addsubmit',function(req,res){
-    let  addSql = 'INSERT INTO user(name,birthday,password)VALUES(?,?,?)';
-    let  addSqlArr = [req.body.name,req.body.birthday,req.body.password];
-    connection.query(addSql,addSqlArr,function (err, result) {
-        if(err){
-            console.log('[SELECT ERROR]:',err.message);
-            res.send('服务器内部错误！');
-            return;
-        }
-        let addsubmitresult=[{id:result.insertId,name:req.body.name}];
-        res.send(addsubmitresult);
-    });
-});
-/**
- * @param {{birthday:string}} data
- */
-router.post('/update',function (req,res) {
-    let idupdatevalue=req.body.id;
-    let nameupdatevalue=req.body.name;
-    let passwordupdatevalue=req.body.password;
-    let birthdayupdatevalue=req.body.birthday;
-    let nametmp,passwordtmp,birthdaytmp,data='';
-    let nameswitch=0,passwordswitch=0,birthdayswitch=0,varswitch=0;
-    let commacount=0;
-    if(!(nameupdatevalue===undefined)){
-        nametmp='name='+"'"+nameupdatevalue+"'";
-        nameswitch=1;
-    }
-    if(!(passwordupdatevalue===undefined)){
-        passwordtmp='password='+"'"+passwordupdatevalue+"'";
-        passwordswitch=1;
-    }
-    if(!(birthdayupdatevalue===undefined)){
-        birthdaytmp='birthday='+"'"+birthdayupdatevalue+"'";
-        birthdayswitch=1;
-    }
 
-    varswitch=nameswitch+passwordswitch+birthdayswitch;
-    commacount=varswitch-1;
-    if(nameswitch===1){
-        if(commacount===0){
-            data+=nametmp;
-        }else{
-            data+=nametmp+",";
-        }
-    }
-    if(passwordswitch===1){
-        if(commacount!==0&&birthdayswitch===1){
-            data+=passwordtmp+",";
-        }else{
-            data+=passwordtmp;
-        }
-    }
-    if(birthdayswitch===1){
-        data+=birthdaytmp;
-    }
-    let updateSql='UPDATE user SET '+data+' WHERE ID = '+idupdatevalue;
-    // let updateSqlArr=[nameupdatevalue,passwordupdatevalue,birthdayupdatevalue,idupdatevalue];
-    connection.query(updateSql,function (err, result) {
-        if(err){
-            console.log('[SELECT ERROR]:',err.message);
-            res.send('服务器内部错误！');
-            return;
-        }
-        let updateresult={id:idupdatevalue};
-        res.send(updateresult);
-    });
+//CRUD
+router.get('/idquery',function (req,res,next) {
+    sqlDefine.crudRetrieve(req,res,next);
 })
 
-router.post('/iddelete',function (req,res) {
-    let iddeletevalue=req.body.id;
-    let deleteSql='DELETE FROM user where ID=?';
-    let deleteSqlArr=[iddeletevalue];
-    let deleteresult='';
-    connection.query(deleteSql,deleteSqlArr,function (err, result) {
-        if(err){
-            console.log('[SELECT ERROR]:',err.message);
-            res.send({errCode:500}); //500服务器内部错误
-            return;
-        }
-        if(result.affectedRows===1){
-            deleteresult={statusCode:200}
-        }else{
-            deleteresult='无该ID记录，删除失败！'
-        }        
-        // if(result.affectedRows===1){
-        //     deleteresult='已成功删除！'
-        // }else{
-        //     deleteresult='无该ID记录，删除失败！'
-        // }
-        res.send(deleteresult);
-    });
+router.post('/addsubmit',function(req,res,next){
+    sqlDefine.crudCreate(req,res,next)
 })
 
-router.post('/login',function(req,res){
-    //res.send('req:');
-    res.send('req:'+util.inspect(req));
+router.post('/update',function (req,res,next) {
+    sqlDefine.crudUpdate(req,res,next)
 })
+
+router.post('/iddelete',function (req,res,next) {
+    sqlDefine.crudDelete(req,res,next)
+})
+
+//auth
+router.post('/login',async function(req,res,next){
+    sqlDefine.crudAuth(req,res,next);
+})
+
+//hiveReadAllEmployee
+router.get('/user/hive_all_employee',function (req,res,next) {
+    sqlDefine.hiveReadAllEmployee(req,res,next);
+})
+
+//hiveGetPost
+router.get('/user/hive_post',function (req,res,next) {
+    sqlDefine.hiveGetPost(req,res,next);
+})
+
+//hiveGetDepartment
+router.get('/user/hive_department',function (req,res,next) {
+    sqlDefine.hiveGetDepartment(req,res,next);
+})
+
+//hivePostMapDepartment
+router.get('/user/hive_post_map_department',function (req,res,next) {
+    sqlDefine.hivePostMapDepartment(req,res,next);
+})
+
+//前端需要的post、department瀑布级联表
+router.get('/user/hive_post_cascader',function (req,res,next) {
+    sqlDefine.hivePostCascader(req,res,next);
+})
+
+//更新员工信息
+router.put('/user/hive_employee',function (req,res,next) {
+    sqlDefine.hiveUpdateEmployee(req,res,next);
+})
+
+//删除员工
+router.delete('/user/hive_employee/:id',function (req,res,next) {
+    sqlDefine.hiveDeleteEmployee(req,res,next);
+})
+
+//创建员工
+router.post('/user/hive_employee',function (req,res,next) {
+    sqlDefine.hiveCreateEmployee(req,res,next);
+})
+
+//测试api
+router.get('/user/hive_test/:id',function (req,res,next) {
+    sqlDefine.hiveTest(req,res,next);
+})
+
+router.get('/get_header_portrait',function(req,res){
+    let username=req.cookies.u;
+    let headPortraitPath=path.format({
+        root: '/',
+        dir: '/usr/local/nginx/html/ftp',
+        base: `${username}.jpg`
+    });
+    fs.stat(headPortraitPath, function (err, stat) {
+        if(err===null){
+            console.log(headPortraitPath);
+            //res.set('Content-Type','image/jpeg')
+            return res.send({statusCode:'200120',tag:1,path:headPortraitPath});
+        }else{
+            return res.send({statusCode:'400120',tag:0});
+        }
+    });
+
+})
+
 module.exports = router;
